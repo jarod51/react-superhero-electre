1c2d852fda8e083d16a26bbef0153718eb4a0d3c
FabioReact <fabio.react.dev@gmail.com>
2022-05-25 15:51:37 +0200

    feat: add memoization to HeroesList component

--- src/components/HeroesList.tsx
+++ src/components/HeroesList.tsx
@@ -1,12 +1,18 @@
+// import { useDeferredValue } from 'react'
+import { memo } from 'react'
 import { Hero } from '../types/hero'
 import HeroCard from './HeroCard'
 
+
 const HeroesList = ({ heroes }: { heroes: Hero[] }) => {
+  console.log('HeroesList chargé')
+  // const deferredHeroes = useDeferredValue(heroes)
   return (
     <div className='flex flex-wrap justify-center gap-6'>
       {heroes.map((hero) => <HeroCard key={hero.id} hero={hero}/>)}
     </div>
   )
+
 }
 
-export default HeroesList
+export default memo(HeroesList)
